4ec3b1be9c88da91bf4078ad9fd11b0d38a39b0b
Viktor <viktor.kiselev1992@gmail.com>
2022-08-15 20:04:55 +0300

    init

--- libgdx_myGame/core/src/v/kiselev/MyGdxGamex.java
+++ libgdx_myGame/core/src/v/kiselev/MyGdxGamex.java
@@ -2,7 +2,6 @@ package v.kiselev;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -10,26 +9,21 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class MyGdxGamex extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	int count;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("smile.png");
+		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 1, 1, 1);
+		ScreenUtils.clear(1, 0, 0, 1);
 
 		int x = Gdx.input.getX() - img.getWidth()/2;
 		int y = Gdx.graphics.getHeight() - Gdx.input.getY() - img.getHeight()/2;
-
-		if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
-			count++;
-		}
-		Gdx.graphics.setTitle("Count click mouse " + count);
 		batch.begin();
+		batch.draw(img, 0, 0);
 		batch.draw(img, x, y);
 		batch.end();
 	}
